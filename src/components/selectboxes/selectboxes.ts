5c170ff3a9e77412c93b787ef567a4bd75f2af25
Manish Kumar <manish.kumar@tudip.nl>
2016-11-22 19:42:07 +0530

    Implementation of reverting the changes for select boxes component. 	Removed the breaking code of select boxes component functionality.

--- src/components/selectboxes/selectboxes.ts
+++ src/components/selectboxes/selectboxes.ts
@@ -1,7 +1,6 @@
 import { BaseComponent, BaseOptions, BaseElement} from '../base';
 import { FormioComponents } from '../components';
 import { FormioTemplate } from '../../formio.template';
-import { OnInit } from '@angular/core';
 
 export interface SelectBoxOptions extends BaseOptions<string> {
     inputType?: string,
@@ -13,22 +12,8 @@ export interface SelectBoxOptions extends BaseOptions<string> {
 }
 
 export class SelectBoxComponent extends BaseComponent<SelectBoxOptions> {}
-export class SelectBoxElement extends BaseElement<SelectBoxComponent> implements OnInit {
-    public submitValue: Object = {};
-    ngOnInit() {
-        if (this.component.data[this.component.settings.key] != null) {
-            this.submitValue = this.component.data[this.component.settings.key];
-        }
-        else {
-            this.component.settings.values.forEach((item: any) => {
-                this.submitValue[item.value] = false;
-            });
-        }
-    }
-    onChange(option: any, isChecked: boolean) {
-        this.submitValue[option.value] = isChecked;
-        this.component.setValue(this.submitValue);
-    }
+export class SelectBoxElement extends BaseElement<SelectBoxComponent> {
+    public selected: Array<string> = [];
 }
 
 export function SelectBoxField(template:FormioTemplate) {
