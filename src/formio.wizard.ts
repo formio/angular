079c6a5f3c24927f8f1694723a2ecc530188d9a9
Manish Kumar <manish.kumar@tudip.nl>
2016-11-29 20:37:06 +0530

    Implementation of formio wizard functionality.(WIP) 	Added loader while form or form wizard is loading. 	Added ready or async checks to render the form or form wizard using src and form attributes. 	Added check error method to check validation errors. 	On next and submit methods if errors are present then prevented the next page and submit actions. 	Working on to get the validation errors in each page of wizard.

--- src/formio.wizard.ts
+++ src/formio.wizard.ts
@@ -1,8 +1,7 @@
-import 'core-js/es7/reflect';
-import {Component, Input, Output, EventEmitter, OnInit }  from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, ElementRef }  from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormioService } from './formio.service';
-import { FormioForm } from './formio.common';
+import { FormioForm, FormioEvents } from './formio.common';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
@@ -11,6 +10,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 })
 export class FormioWizardComponent implements OnInit {
     public formGroup: FormGroup = new FormGroup({});
+    public events: FormioEvents = new FormioEvents();
     public ready: BehaviorSubject<boolean> = new BehaviorSubject(false);
     public page: any;
     public pages: Array<any> = [];
@@ -22,24 +22,18 @@ export class FormioWizardComponent implements OnInit {
     @Input() src: string;
     @Input() service: FormioService;
     @Output() change: EventEmitter<any> = new EventEmitter();
+
+    constructor(private elementRef: ElementRef) {}
     ngOnInit() {
         this.currentPage = 0;
         this.page = this.form.components[0];
-        if (this.form) {
-            this.ready.next(true);
-        }
-        else if (this.src && !this.service) {
-            this.service = new FormioService(this.src);
-            this.service.loadForm().subscribe((form: FormioForm) => {
-                if (form && form.components) {
-                    this.form = form;
-                    this.ready.next(true);
-                }
-            });
-        }
         this.form.components.forEach((item: any) => {
             this.pages.push(item);
         });
+        if (this.src) {
+            this.service = new FormioService(this.src);
+            this.pages.splice(this.pages.length-1, 1);
+        }
     }
     onChange(page: any, event: any) {
         this.storage['page'] = this.pages.indexOf(page) + 1;
@@ -47,7 +41,14 @@ export class FormioWizardComponent implements OnInit {
         this.storage['data'] = this.data;
         localStorage.setItem('wizard', JSON.stringify(this.storage));
     }
+    public checkErrors(): boolean {
+        //@TODO: Check validations...
+        return false;
+    }
     public next() {
+        if (this.checkErrors()) {
+            return;
+        }
         if (this.currentPage >= (this.pages.length - 1)) {
             return;
         }
@@ -55,6 +56,7 @@ export class FormioWizardComponent implements OnInit {
         this.page = this.pages[this.currentPage];
     }
     public prev() {
+        this.submission = this.data;
         if (this.currentPage < 1) {
             return;
         }
@@ -62,6 +64,9 @@ export class FormioWizardComponent implements OnInit {
         this.page = this.pages[this.currentPage];
     }
     public onSubmitWizard() {
+        if (this.checkErrors()) {
+            return;
+        }
         localStorage.setItem('wizard','');
         let submission = {data: this.data};
         if (this.service) {
