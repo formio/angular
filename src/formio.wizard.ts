4ea389f080e7409943fee7c3ca1372d72a0a7883
Manish Kumar <manish.kumar@tudip.nl>
2016-12-01 20:17:48 +0530

    Implementation of formio wizard functionality.(WIP) 	Added function to add styles to progress bar based on number of pages. 	Added data entered in submission attribute to get the values entered on previous pages.

--- src/formio.wizard.ts
+++ src/formio.wizard.ts
@@ -17,6 +17,9 @@ export class FormioWizardComponent implements OnInit {
     public currentPage: number;
     public storage: Object = {};
     public data: Object = {};
+    public margin: any;
+    public colClass: string;
+    public localStorageKey: string;
     @Input() form: FormioForm = null;
     @Input() submission: any = {};
     @Input() src: string;
@@ -30,16 +33,20 @@ export class FormioWizardComponent implements OnInit {
         this.form.components.forEach((item: any) => {
             this.pages.push(item);
         });
+        this.updatePages();
         if (this.src) {
+            this.localStorageKey = this.src.split('/')[3];
             this.service = new FormioService(this.src);
             this.pages.splice(this.pages.length-1, 1);
+        }
+        else {
+            this.localStorageKey = 'form_wizard';
         }
     }
     onChange(page: any, event: any) {
         this.storage['page'] = this.pages.indexOf(page) + 1;
         this.data[event.target.id] = event.target.value;
         this.storage['data'] = this.data;
-        localStorage.setItem('wizard', JSON.stringify(this.storage));
     }
     public checkErrors(): boolean {
         //@TODO: Check Validations...
@@ -53,21 +60,22 @@ export class FormioWizardComponent implements OnInit {
             return;
         }
         this.currentPage++;
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.storage));
         this.page = this.pages[this.currentPage];
     }
     public prev() {
-        this.submission = this.data;
         if (this.currentPage < 1) {
             return;
         }
         this.currentPage--;
         this.page = this.pages[this.currentPage];
+        this.submission.data = this.data;
     }
     public onSubmitWizard() {
         if (this.checkErrors()) {
             return;
         }
-        localStorage.setItem('wizard','');
+        localStorage.setItem(this.localStorageKey,'');
         let submission = {data: this.data};
         if (this.service) {
             this.service.saveSubmission(submission).subscribe((sub: {}) => {});
@@ -78,5 +86,15 @@ export class FormioWizardComponent implements OnInit {
         if (index >= this.pages.length) {return;}
         this.currentPage = index;
         this.page = this.pages[this.currentPage];
+    }
+    public updatePages() {
+        if (this.pages.length > 6) {
+            this.margin = ((1 - (this.pages.length * 0.0833333333)) / 2) * 100;
+            this.colClass = 'col-sm-1';
+        }
+        else {
+            this.margin = ((1 - (this.pages.length * 0.1666666667)) / 2) * 100;
+            this.colClass = 'col-sm-2';
+        }
     }
 }
