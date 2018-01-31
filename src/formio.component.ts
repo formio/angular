8e6666542e2c4f420df81501ff3e58aef8cc542b
Edwin Anciani <edwinanciani@gmail.com>
2018-01-30 18:54:16 -0600

    Component Rendering Issue In Prod Build

--- src/formio.component.ts
+++ src/formio.component.ts
@@ -36,7 +36,7 @@ const _isEmpty = require('lodash/isEmpty');
     '<formio-alerts *ngIf="!this.options.disableAlerts" [alerts]="alerts"></formio-alerts>' +
     '<div #formio></div>' +
     '</div>',
-  styles: [require('./formio.component.scss').toString()],
+  styleUrls: ['./formio.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class FormioComponent implements OnInit, OnChanges {
