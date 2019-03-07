a69df3c04dd90ed915ad1204de366a5109c37aa9
Travis Tidwell <travis@form.io>
2019-03-07 14:34:21 -0600

    Fixing the embed code feature.

--- src/manager/form/form.component.ts
+++ src/manager/form/form.component.ts
@@ -10,10 +10,9 @@ import { Formio } from 'formiojs';
   templateUrl: './form.component.html'
 })
 export class FormManagerFormComponent implements OnInit {
-  choice: any = 'isEmbed';
+  choice: any = 'isUrl';
   embedCode: any;
   formio: any;
-  formUrl: any;
   src: any;
   shareUrl: any;
   projectId: any;
@@ -30,8 +29,6 @@ export class FormManagerFormComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      const appUrl = window.location.protocol + '//' + window.location.host + window.location.pathname;
-      this.formUrl = appUrl;
       this.formio = new Formio(`${this.appConfig.appUrl}/form/${params.id}`);
       this.formio.loadForm().then(form => {
         this.projectId = form.project;
@@ -43,13 +40,13 @@ export class FormManagerFormComponent implements OnInit {
 
   openEmbed(content: TemplateRef<any>) {
     this.src = this.appConfig.appUrl + '/' + this.pathName;
-    this.shareUrl = `${this.options.viewer}#/?src=${encodeURIComponent(this.src)}&theme=cosmo&header=0&iframe=1`;
+    this.shareUrl = `${this.options.viewer}#/?src=${encodeURIComponent(this.src)}`;
     let embedCode = '<script type="text/javascript">';
     embedCode += '(function a(d, w, u) {';
     embedCode +=    'var h = d.getElementsByTagName("head")[0];';
     embedCode +=    'var s = d.createElement("script");';
     embedCode +=    's.type = "text/javascript";';
-    embedCode +=    's.src = "' + window.location.protocol + '//' + window.location.host + window.location.pathname + 'lib/seamless/seamless.parent.min.js";';
+    embedCode +=    's.src = "' + this.options.viewer + '/assets/lib/seamless/seamless.parent.min.js";';
     embedCode +=    's.onload = function b() {';
     embedCode +=       'var f = d.getElementById("formio-form-' + this.formio.formId + '");';
     embedCode +=       'if (!f || (typeof w.seamless === u)) {';
@@ -62,7 +59,7 @@ export class FormManagerFormComponent implements OnInit {
     embedCode +=    'h.appendChild(s);';
     embedCode += '})(document, window);';
     embedCode += '</script>';
-    embedCode += '<iframe id="formio-form-' + this.formio.formId + '" style="width:100%;border:none;" height="800px" src="' + this.shareUrl + '"></iframe>';
+    embedCode += '<iframe id="formio-form-' + this.formio.formId + '" style="width:100%;border:none;" height="800px" src="' + this.shareUrl + '&iframe=1"></iframe>';
     this.embedCode = embedCode;
     this.modalRef = this.modalService.show(content, { class: 'modal-lg' });
   }
