e4649e297a3c133a06211e6461e8ea0cc6cc1a0b
Travis Tidwell <travis@form.io>
2020-01-22 12:26:28 -0600

    Updated build and version.

---
+++ src/core.ts
@@ -0,0 +1,21 @@
+export * from './formio.config';
+export * from './formio.common';
+export * from './formio.service';
+export * from './formio.utils';
+export * from './FormioBaseComponent';
+export * from './components/formio/formio.component';
+export * from './components/formbuilder/formbuilder.component';
+export * from './components/loader/formio.loader';
+export * from './components/loader/formio.loader.component';
+export * from './components/alerts/formio.alerts';
+export * from './components/alerts/formio.alerts.component';
+export { FormioModule } from './formio.module';
+export {
+  Formio,
+  Utils as FormioUtils,
+  Templates,
+  Components,
+  ComponentSchema,
+  ExtendedComponentSchema,
+  ElementInfo
+} from 'formiojs';
