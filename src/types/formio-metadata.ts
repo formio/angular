0e19be210be477277ab33b7b16a08e24a5a4c584
Vlad <vlad.shusterman30@gmail.com>
2020-05-22 17:02:27 +0300

    Feat(FormioMetadataInterface)

---
+++ src/types/formio-metadata.ts
@@ -0,0 +1,9 @@
+export default interface FormioMetadata {
+  timezone?: string;
+  offset?: number;
+  referrer?: string;
+  browserName?: string;
+  userAgent?: string;
+  pathName?: string;
+  onLine?: boolean;
+}
