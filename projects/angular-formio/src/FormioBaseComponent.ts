e33ebc3b8041c49ebfa77fad8ed89ab5a37fe9df
mikekotikov <kmikecoder@gmail.com>
2022-06-17 15:47:48 +0300

    fix validation errors shown on form load

--- projects/angular-formio/src/FormioBaseComponent.ts
+++ projects/angular-formio/src/FormioBaseComponent.ts
@@ -323,7 +323,9 @@ export class FormioBaseComponent implements OnInit, OnChanges, OnDestroy {
 
     this.formioReady.then(() => {
       if (changes.submission && changes.submission.currentValue) {
-        this.formio.setSubmission(changes.submission.currentValue);
+        this.formio.setSubmission(changes.submission.currentValue, {
+          fromSubmission: !changes.submission.firstChange
+        });
       }
 
       if (changes.hideComponents && changes.hideComponents.currentValue) {
