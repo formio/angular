3a790a91e1b16e6210a7464a8dc8f54748359c21
Travis Tidwell <travis@form.io>
2023-03-02 18:13:24 -0600

    Fixing the ui to work with bootstrap 5.

--- projects/angular-formio/grid/src/form/FormGridBody.component.ts
+++ projects/angular-formio/grid/src/form/FormGridBody.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { GridBodyComponent } from '../GridBodyComponent';
 import { FormioPromiseService } from '@formio/angular';
-
+import { Tooltip } from 'bootstrap';
 @Component({
   selector: 'form-grid-body',
   styleUrls: ['./FormGridBody.component.scss'],
   templateUrl: './FormGridBody.component.html'
 })
-export class FormGridBodyComponent extends GridBodyComponent {
+export class FormGridBodyComponent extends GridBodyComponent implements OnDestroy {
+  @ViewChild('create') createBtn: ElementRef;
+  @ViewChild('view') viewBtn: ElementRef;
+  @ViewChild('edit') editBtn: ElementRef;
+  @ViewChild('permissions') permissionsBtn: ElementRef;
+  @ViewChild('delete') deleteBtn: ElementRef;
+  public tooltips: Array<Tooltip> = [];
   load(formio: FormioPromiseService, query?: any) {
     query = query || {};
-    return formio.loadForms({ params: query }).then((forms: any) => this.setRows(query, forms));
+    return formio.loadForms({ params: query })
+      .then((forms: any) => this.setRows(query, forms))
+      .then(() => {
+        this.tooltips.push(new Tooltip(this.createBtn.nativeElement, {
+          title: 'Enter Data'
+        }));
+        this.tooltips.push(new Tooltip(this.viewBtn.nativeElement, {
+          title: 'View Data'
+        }));
+        this.tooltips.push(new Tooltip(this.editBtn.nativeElement, {
+          title: 'Edit Form'
+        }));
+        this.tooltips.push(new Tooltip(this.permissionsBtn.nativeElement, {
+          title: 'Permissions'
+        }));
+        this.tooltips.push(new Tooltip(this.deleteBtn.nativeElement, {
+          title: 'Delete form'
+        }));
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.tooltips.forEach((tootip: Tooltip) => tootip.dispose());
   }
 }
