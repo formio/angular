4519fc8bb716cf5b533bd3eca272958a446840e3
Travis Tidwell <travis@form.io>
2024-06-10 10:15:19 -0500

    Updated build.

--- projects/angular-formio/resource/src/resource.service.ts
+++ projects/angular-formio/resource/src/resource.service.ts
@@ -85,8 +85,9 @@ export class FormioResourceService {
   }
 
   init(route: ActivatedRoute) {
-    const reset = route.snapshot.queryParams.hasOwnProperty('reset') ? route.snapshot.queryParams.reset : false;
-    const resourceId = route.snapshot.params['id'];
+    const snapshot = route.snapshot;
+    const reset = snapshot.queryParams?.hasOwnProperty('reset') ? snapshot.queryParams.reset : false;
+    const resourceId = snapshot.params['id'];
     if (resourceId && (resourceId === this.resourceId) && !reset) {
       return this.ready;
     }
