7d39e80b0b585badb09ac2e58d8a794b88871dc7
Travis Tidwell <travis@form.io>
2024-07-30 08:58:52 -0500

    Revert "Removing inject from the angular app config class."
    This reverts commit ae036dcde53e7a0d5a3095b112f187b854f6e02d.

--- projects/angular-formio/src/formio.config.ts
+++ projects/angular-formio/src/formio.config.ts
@@ -1,16 +1,23 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
+export const FORMIO_CONFIG = new InjectionToken('formio-config');
 import { Formio } from '@formio/js';
-@Injectable({
-  providedIn: 'root'
-})
-export class FormioAppConfig implements OnInit {
+
+@Injectable()
+export class FormioAppConfig {
   [x: string]: any;
   appUrl = '';
   apiUrl = '';
   icons?: string;
   formOnly?: boolean;
   formio?: Formio;
-  ngOnInit(): void {
+  constructor(@Inject(FORMIO_CONFIG) config: {
+    apiUrl?: string,
+    baseUrl?: string, 
+    appUrl?: string,
+    projectUrl?: string 
+  } = {}) {
+    this.apiUrl = config.apiUrl || config.baseUrl;
+    this.appUrl = config.appUrl || config.projectUrl;
     if (this.apiUrl) {
       Formio.setBaseUrl(this.apiUrl);
       Formio.setProjectUrl(this.appUrl);
