9140195a28c48035569cf9e3dd481416a84fefa4
Travis Tidwell <travis@form.io>
2024-10-30 18:05:00 -0500

    Expanding the components to allow for more functionality.

--- projects/angular-formio/embed/src/formio.component.ts
+++ projects/angular-formio/embed/src/formio.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef, Input, Output, EventEmitter, OnChanges, AfterViewInit } from '@angular/core';
-import { FormioCore as Formio } from '@formio/js';
+import { FormioCore as Formio, Webform } from '@formio/js';
+import { Form, Submission } from '@formio/core/types'
 
 @Component({
     selector: 'formio',
@@ -7,21 +8,29 @@ import { FormioCore as Formio } from '@formio/js';
 })
 export class FormioComponent implements AfterViewInit {
     @Input() src?: string;
-    @Input() form?: Object | null;
-    @Input() submission?: Object | null;
-    @Input() options?: Object = {};
-    @Output() ready = new EventEmitter<Formio>();
+    @Input() form?: Form | null;
+    @Input() submission?: Submission | null;
+    @Input() url?: string;
+    @Input() options?: Webform["options"] = {};
+    @Output() ready = new EventEmitter<Webform>();
     @Output() submit = new EventEmitter<object>();
     @Output() error = new EventEmitter<any>();
+    @Output() change = new EventEmitter<any>();
     @ViewChild('formio') element: ElementRef;
+    public instance: Webform;
     ngAfterViewInit(): void {
-        Formio.createForm(this.element.nativeElement, this.src || this.form, this.options).then((form) => {
+        Formio.createForm(this.element.nativeElement, this.src || this.form, this.options).then((form: Webform) => {
+            this.instance = form;
+            if (this.url) {
+                form.url = this.url;
+            }
             if (this.submission) {
                 form.submission = this.submission;
             }
             this.ready.emit(form);
             form.on('submit', (submission) => this.submit.emit(submission));
             form.on('error', (err) => this.error.emit(err));
+            form.on('change', (event) => this.change.emit(event));
         }).catch((err) => this.error.emit(err));
     }
 }
