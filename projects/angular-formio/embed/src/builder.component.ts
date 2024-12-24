bee3c827ddb557fba331e33ffb7aaee19ab5ea6f
lane-formio <lane@form.io>
2024-12-24 09:30:02 -0600

    Merge pull request #1105 from formio/expand-embed-lib
    Expanding the components to allow for more functionality.

--- projects/angular-formio/embed/src/builder.component.ts
+++ projects/angular-formio/embed/src/builder.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, Input, ViewChild, Output, EventEmitter, AfterViewInit } from '@angular/core';
-import { FormioCore as Formio } from '@formio/js';
+import { FormBuilder } from '@formio/js';
+import WebformBuilder from '@formio/js/lib/cjs/WebformBuilder';
 
 @Component({
     selector: 'formio-builder',
@@ -9,11 +10,59 @@ export class FormioBuilder implements AfterViewInit {
     @ViewChild('formio') element: ElementRef;
     @Input() form?: Object | null;
     @Input() options?: Object = {};
-    @Output() ready = new EventEmitter<Formio>();
+    @Output() change = new EventEmitter<any>();
+    @Output() ready = new EventEmitter<any>();
     @Output() error = new EventEmitter<any>();
+    public builder: FormBuilder;
+    public componentAdding = false;
+    get instance(): WebformBuilder {
+        return this.builder.instance;
+    }
     ngAfterViewInit(): void {
-        Formio.builder(this.element.nativeElement, this.form, this.options).then((builder) => {
-            this.ready.emit(builder);
+        this.builder = new FormBuilder(this.element.nativeElement, this.form, this.options);
+        this.builder.ready.then(() => {
+            this.instance.on('addComponent', (component, parent, path, index, isNew) => {
+                if (isNew) {
+                    this.componentAdding = true;
+                } else {
+                    this.change.emit({
+                        type: 'addComponent',
+                        builder: this.instance,
+                        form: this.instance.schema,
+                        component: component,
+                        parent: parent,
+                        path: path,
+                        index: index
+                    });
+                    this.componentAdding = false;
+                }
+            });
+            this.instance.on('saveComponent', (component, original, parent, path, index, isNew) => {
+                this.change.emit({
+                    type: this.componentAdding ? 'addComponent' : 'saveComponent',
+                    builder: this.instance,
+                    form: this.instance.schema,
+                    component: component,
+                    originalComponent: original,
+                    parent: parent,
+                    path: path,
+                    index: index,
+                    isNew: isNew || false
+                });
+                this.componentAdding = false;
+            });
+            this.instance.on('removeComponent', (component, parent, path, index) => {
+                this.change.emit({
+                    type: 'deleteComponent',
+                    builder: this.instance,
+                    form: this.instance.schema,
+                    component: component,
+                    parent: parent,
+                    path: path,
+                    index: index
+                });
+            });
+            this.ready.emit(this.instance);
         }).catch((err) => this.error.emit(err));
     }
 }
