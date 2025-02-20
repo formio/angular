2668ffff419caf0c78f3c074c4cbf8d4d6dff6a9
Brendan J Bond <brendanbond@gmail.com>
2025-02-20 09:56:09 -0600

    remove experimental embed directory

--- projects/angular-formio/embed/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { FormioCore as Formio } from '@formio/js';
-export { FormioComponent } from './formio.component';
-export { FormioBuilder } from './builder.component';
-export { FormioEmbedModule } from './embed.module';
-export { FormioAppService, FormioAppConfig } from './app.service';
-export { Formio };
